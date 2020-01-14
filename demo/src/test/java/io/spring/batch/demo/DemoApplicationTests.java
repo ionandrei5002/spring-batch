7ba6dda3cf6207b3b069805c5ae5f6d67446f605
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2020-01-14 17:07:34 +0200

    first commit

---
+++ demo/src/test/java/io/spring/batch/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.batch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
