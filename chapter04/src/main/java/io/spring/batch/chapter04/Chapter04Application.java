e9bfb9b2003c573efe77913951ad15ebec4612b2
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2020-01-15 15:54:26 +0200

    +chapter04

---
+++ chapter04/src/main/java/io/spring/batch/chapter04/Chapter04Application.java
@@ -0,0 +1,91 @@
+package io.spring.batch.chapter04;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class Chapter04Application {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public CompositeJobParametersValidator validator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+
+        DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(
+            new String[] {"fileName"},
+            new String[] {"name", "currentDate"}
+        );
+
+        defaultJobParametersValidator.afterPropertiesSet();
+
+        validator.setValidators(
+                Arrays.asList(new ParameterValidator(), defaultJobParametersValidator)
+        );
+
+        return validator;
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("basicJob")
+                .start(step1())
+                .validator(validator())
+                .incrementer(new DailyJobTimestamper())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet(helloWorldTasklet(null, null))
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['fileName']}") String fileName
+    ) {
+            return (contribution, chunkContext) -> {
+                System.out.println(String.format("Hello, %s", name));
+                System.out.println(String.format("fileName = %s", fileName));
+                return RepeatStatus.FINISHED;
+            };
+
+//        return (contribution, chunkContext) -> {
+//            String name = (String) chunkContext.getStepContext()
+//                    .getJobParameters()
+//                    .get("name");
+//            System.out.println(String.format("Hello, %s!", name));
+//            return RepeatStatus.FINISHED;
+//        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter04Application.class, args);
+    }
+
+}
---
+++ chapter04/src/main/java/io/spring/batch/chapter04/ParameterValidator.java
@@ -0,0 +1,19 @@
+package io.spring.batch.chapter04;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.util.StringUtils;
+
+public class ParameterValidator implements JobParametersValidator {
+    @Override
+    public void validate(JobParameters jobParameters) throws JobParametersInvalidException {
+        String fileName = jobParameters.getString("fileName");
+        if(!StringUtils.hasText(fileName)) {
+            throw new JobParametersInvalidException("fileName parameter is nissing");
+        }
+        else if(!StringUtils.endsWithIgnoreCase(fileName, "csv")) {
+            throw new JobParametersInvalidException("filename parameter does not use the csv file extension");
+        }
+    }
+}
